92eee043a04fc5dd78c0f6b016b525e319de28da
ARAVINDH A <aravindhjoshua997@gmail.com>
2026-01-24 12:33:40 +0530

    Update backtest.py

--- src/backtest.py
+++ src/backtest.py
@@ -1,1 +1,7 @@
+import numpy as np
+import pandas as pd
 
+def backtest_strategy(returns, positions):
+    strat_returns = returns.values * positions
+    equity_curve = (1 + strat_returns).cumprod()
+    return equity_curve
