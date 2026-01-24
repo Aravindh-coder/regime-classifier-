5e836a3148e3460067b44541a5e1f0e5d5bbf262
ARAVINDH A <aravindhjoshua997@gmail.com>
2026-01-24 12:32:37 +0530

    Update regime_predictor.py

--- src/regime_predictor.py
+++ src/regime_predictor.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+def transition_matrix(hmm_model):
+    return hmm_model.transmat_
+
+def predict_next_regime(current_regime, transmat):
+    return np.argmax(transmat[current_regime])
+
+def regime_probabilities(hmm_model):
+    return hmm_model.predict_proba(hmm_model._compute_log_likelihood)
